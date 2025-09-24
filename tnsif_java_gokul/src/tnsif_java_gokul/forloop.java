874eb561997287c5a95727aaf335c93d186febfb
Gokul <gokulr0903@gmail.com>
2025-09-24 16:00:42 +0530

    commit 2

---
+++ tnsif_java_gokul/src/tnsif_java_gokul/forloop.java
@@ -0,0 +1,16 @@
+
+/*
+package tnsif_java_gokul;
+
+public class forloop {
+	public static void main(String[] args) {
+	
+	for(int i=1; i<10; i++) {
+		if(i == 9)
+			break;
+		System.out.println(i);
+	}
+	}
+
+}
+*/
