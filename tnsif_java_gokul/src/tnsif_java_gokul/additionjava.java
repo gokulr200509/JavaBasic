c7655f8c1dd3f03bb85e37414ffb26c82878f799
Gokul <gokulr0903@gmail.com>
2025-09-26 14:39:26 +0530

    commit 4

---
+++ tnsif_java_gokul/src/tnsif_java_gokul/additionjava.java
@@ -0,0 +1,17 @@
+package tnsif_java_gokul;
+
+import java.util.Scanner;
+
+public class additionjava {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the 1st Number:");
+		int a = sc.nextInt();
+		System.out.print("Enter the 2snd Number:");
+		int b = sc.nextInt();
+		int c = a + b;
+		System.out.println(c);
+	}
+
+}
---
+++ tnsif_java_gokul/src/tnsif_java_gokul/info.java
@@ -0,0 +1,11 @@
+package tnsif_java_gokul;
+import java.util.Scanner;
+
+public class info {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
