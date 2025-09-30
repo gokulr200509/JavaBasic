b00e1064c025a5895988c39301e448b979a50cc2
Gokul <gokulr0903@gmail.com>
2025-09-30 10:20:51 +0530

    adding abstract file.

---
+++ tnsif_java_gokul/src/tnsif_java_day4/EncalsulationDem.java
@@ -0,0 +1,19 @@
+package tnsif_java_day4;
+
+public class EncalsulationDem {
+
+
+	public class EncapsulationDemo {
+		public static void main(String[] args) {
+			
+			
+			//object creation 
+			OopsConceptDemo obj = new OopsConceptDemo();
+			obj.setSerialNum(101);
+			obj.setName("Pooja");
+			obj.setAge(20);
+			System.out.println(obj);
+			
+		}
+
+	}
